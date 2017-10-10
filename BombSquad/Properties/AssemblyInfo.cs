e7052c9acef15f750f321a816ce6eb29cadbf51d
Stephen Powell <steve.powell36@gmail.com>
2017-10-11 00:52:47 +1030

    App info changes

--- BombSquad/Properties/AssemblyInfo.cs
+++ BombSquad/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BombSquad")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription( "Minesweeper Clone" )]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany( "Wildebeest" )]
 [assembly: AssemblyProduct("BombSquad")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyCopyright( "Copyright © Wildebeest 2017" )]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
